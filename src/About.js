1ab223bad764a7ccbfc24b9d8da71e8e96c47e91
Abhishekkashyap007 <ak555585955@gmail.com>
2024-10-27 23:39:29 +0530

    create new project

---
+++ src/About.js
@@ -0,0 +1,18 @@
+const b=<h1>this is js file</h1>
+function Abc()
+{
+    // return "this is webppppppppppppppppp";
+    // return <h1>this is webppppppppppppppp</h1>;
+    return (
+        <div>
+        <h1>this is heading</h1>
+        <h2>this is second heading</h2>
+        </div>
+    )
+}
+export default Abc;
+
+export function Service()
+{
+    return <h4>this is heading four</h4>
+}
