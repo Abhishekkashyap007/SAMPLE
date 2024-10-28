c1442f6883a9acee2e8781cb33c96d3f504eefe2
Abhishekkashyap007 <ak555585955@gmail.com>
2024-10-28 22:50:57 +0530

    image source

--- src/About.js
+++ src/About.js
@@ -1,13 +1,20 @@
+import { Fragment } from "react";
+import myimage from "./phtos/img2.jpg";
+const cdnimg="https://th.bing.com/th/id/OIP.mZABWYIprPBvppX3ZEqc3AAAAA?w=275&h=406&rs=1&pid=ImgDetMain";
+
 const b=<h1>this is js file</h1>
 function Abc()
 {
     // return "this is webppppppppppppppppp";
     // return <h1>this is webppppppppppppppp</h1>;
     return (
-        <div>
-        <h1>this is heading</h1>
+        <fragment>
+        <p>this is <h2>sdfh</h2> heading</p>
         <h2>this is second heading</h2>
-        </div>
+        <img src={myimage} alt="image54545" width="300"/>
+        <img src="phtos/img2.jpg" alt="djsdkfdjk" width="300"/>
+        <img src={cdnimg} alt="dhshdjd" width="300"/>
+        </fragment>
     )
 }
 export default Abc;
