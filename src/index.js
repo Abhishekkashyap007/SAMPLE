65b8592c65344b3f6b0eb90883f9e70e8868529e
Abhishekkashyap007 <ak555585955@gmail.com>
2024-11-01 21:05:29 +0530

    hooks

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import Abc from './About';
 import Xyz,{Contact1} from './Contactus.ts';
+import Myfunc from './Myfunctioncomp.js';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -9,6 +10,7 @@ root.render(
   <Abc></Abc>
   <Xyz></Xyz>
   <Contact1></Contact1>
+  <Myfunc/>
   </React.StrictMode>
 );
 
