082e90cd7f90056811f44404ea909bc3f6a8d557
Abhishekkashyap007 <ak555585955@gmail.com>
2024-12-03 20:27:24 +0530

    change

--- src/modules/payments/Payhomepage.js
+++ src/modules/payments/Payhomepage.js
@@ -1,10 +1,47 @@
 import React from 'react'
-import { Outlet } from 'react-router-dom'
+import axios from 'axios'
+import { useState } from 'react'
+import { useEffect } from 'react'
 
 function Payhomepage() {
+
+    const [mydata, myupdatedata] = useState([])
+
+    const myapi = () => {
+        axios.get('https://jsonplaceholder.typicode.com/posts').then((d) => {
+            console.log(d.data);
+            myupdatedata(d.data)
+        });
+    }
+
+    useEffect(()=>{
+        myapi();
+    },[])
     return (
         <div className='mainpage'>
-            Payhomepage
+            call api in react js
+            <input type="button" value="call api" onClick={myapi} />
+            <div className='container'>
+                <div className='row'>
+                    {mydata.map((m) => {
+                        return (
+                            <div className='col-md-4 mt-3'>
+                                <div className="card bg-info">
+                                    <div className="card-body">
+                                        <h5 className="card-title">{m.id}</h5>
+                                        <h5>{m.title}</h5>
+                                        <p className="card-text">{m.body}</p>
+                                        <a href="#" className="btn btn-primary">Go somewhere</a>
+                                    </div>
+                                </div>
+                            </div>
+                        )
+                    })}
+
+
+
+                </div>
+            </div>
 
         </div>
     )
